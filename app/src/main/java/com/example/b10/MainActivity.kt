485925ffd12d8e4b1e12d1f0cce7e565bed3c9d5
Xuan_Truong <bichkhoai2001@gmail.com>
2022-06-27 09:21:47 +0700

    lan1

--- app/src/main/java/com/example/b10/MainActivity.kt
+++ app/src/main/java/com/example/b10/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Xin chào bạn đây là bài test push lên GitHub
+        //Lần 1
     }
 }
